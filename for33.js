55390d544fa5ca27d343c4a02217d456f77dec21
Doniyor <doniyor19930824@gmail.com>
2022-06-11 21:15:59 +0300

    second commit

--- for33.js
+++ for33.js
@@ -1,10 +1,16 @@
-// 1,1,2,3,5,8,13,21,34,55,89
+// 1,1,2,3,5,8,13,21,34,55, 89
 
-let n = 10;
+let n = 11;
+
 let a = 0;
-let b = 0;
-let sum = 0;
+let b = 1;
+let c = 1;
+let sum = 1;
+console.log(`fibonacci 1 - hadi 1 ga teng`);
 
-for (let i = 0; i < n; i++) {
-    
+for (let i = 2; i <= n; i++) {
+    c = a + b;
+    a = b;
+    b = c;
+    console.log(`fibonacci ${i} - hadi ${c} ga teng`);
 }
